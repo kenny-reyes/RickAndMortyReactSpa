6fcea0e31f217602c879fec0155656d8b56eea03
Kenny <keniakos@hotmail.com>
2020-01-17 07:27:07 +0100

    Reducer working

--- src/redux/CharsDuck.js
+++ src/redux/CharsDuck.js
@@ -1,18 +1,48 @@
+import axios from "axios";
+
 // Constants
 let initialData = {
   fetching: false,
   array: [],
   current: {}
 };
+let URL = "https://rickandmortyapi.com/api/character";
+let GET_CHARACTERS = "GET_CHARACTERS";
+let GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
+let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR";
 
 // Reducers
 export default function reducer(state = initialData, action) {
   switch (action.type) {
+    case GET_CHARACTERS:
+      return { ...state, fetching: true };
+    case GET_CHARACTERS_ERROR:
+      return { ...state, fetching: false, error: action.payload };
+    case GET_CHARACTERS_SUCCESS:
+      return { ...state, array: action.payload, fetching: false };
     default:
       return state;
   }
 }
-// Actions
-export function getCharactersAction() {
-  return dispatch, getState;
-}
+
+// Actions (o thunks)
+export const getCharactersAction = () => (dispatch, getState) => {
+  dispatch({
+    type: GET_CHARACTERS
+  });
+  return axios
+    .get(URL) // la info viene en res.data.results
+    .then(res => {
+      dispatch({
+        type: GET_CHARACTERS_SUCCESS,
+        payload: res.data.results
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({
+        type: GET_CHARACTERS_ERROR,
+        payload: err.response.message
+      });
+    });
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import userReducer from "./userDuck";
+import charsReducer, { getCharactersAction } from "./charsDuck";
 import thunk from "redux-thunk";
 
 let rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  character: charsReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -13,5 +15,8 @@ export default function genareteStore() {
     rootReducer,
     composeEnhancers(applyMiddleware(thunk))
   );
+  // Get the characters the first time
+
+  getCharactersAction()(store.dispatch, store.getState);
   return store;
 }
