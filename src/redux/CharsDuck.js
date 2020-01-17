78d62dc8903eb0f438d5dc06cfd2e7a1c8f51e02
Kenny <keniakos@hotmail.com>
2020-01-17 01:12:47 +0100

    Add Redux, Stores and Ducks

---
+++ src/redux/CharsDuck.js
@@ -0,0 +1,18 @@
+// Constants
+let initialData = {
+  fetching: false,
+  array: [],
+  current: {}
+};
+
+// Reducers
+export default function reducer(state = initialData, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+// Actions
+export function getCharactersAction() {
+  return dispatch, getState;
+}
---
+++ src/redux/userDuck.js
@@ -0,0 +1,16 @@
+// constants
+let initialData = {
+  loggedIn: false
+};
+let LOGIN = "LOGIN";
+
+// reducer
+export default function reducer(state = initialData, action) {
+  switch (action.type) {
+    case LOGIN:
+    default:
+      return state;
+  }
+}
+
+// actions or actions creator
